d83c5c354823550f547a1356a28b13182f59a20d
pipe2442 <felipeangulo2442@gmail.com>
2021-05-25 11:16:00 -0500

    Basic setup with new player object and game loop

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
-const test = () => {
-    document.getElementById('display').innerHTML = "Im the inner"
-};
+    
+    var spot = function getId(spot) { spot.id };
+    
+    let game = false;
+    let turn = 0;
+
+    const player_name1 = document.getElementById('firstplayer');
+    const player_name2 = document.getElementById('secondplayer');
+
+    function Player(name, marker) {
+        this.name = name
+        this.marker = marker
+    }
+    
+    const player1 = new Player(player_name1, 'x')
+    const player2 = new Player(player_name2, 'o')
+
+    const newGame = () => { game = true };
+    
+    while(game) {
+        if (turn % 2 == 0) {
+            document.getElementById(spot).innerHTML = 'x' 
+        } else {
+            document.getElementById(spot).innerHTML = 'o' 
+        }
+
+        if(turn > 3) {game = false};
+        turn += 1;
+    };
