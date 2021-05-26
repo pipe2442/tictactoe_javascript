27407f464c09ee93c160097e3b535544205ef3b5
pipe2442 <felipeangulo2442@gmail.com>
2021-05-26 11:52:32 -0500

    Create check_winner function to review game winner

--- app.js
+++ app.js
@@ -1,35 +1,61 @@
 var oriBoard;
+var game = false;
+var turn_number = 0;
 const huPlayer = 'o';
 const aiPlayer = 'x'
-const winCombos = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [6,4,2]
-];
+var huPlayer_moves  = []
+var aiPlayer_moves  = []
 
 const cells = document.querySelectorAll('.cell');
 startGame();
 
 function startGame() {
-   
-    origBoard = Array.from(Array(9).keys()) // create array 1 to 0
+    origBoard = [0, 1, 2, 3, 4, 5, 6, 7, 8]
     for (var i = 0; i < cells.length; i++){
         cells[i].innerText = ""
-        cells[i].style.removeProperty('background-color');
-        cells[i].addEventListener('click', turnClick, false);
+        cells[i].addEventListener('click', turnClick);
     }
 }
 
 function turnClick(square){
-    turn(square.target.id, huPlayer)
+    game = true;
+    while(game) {
+        if(turn_number % 2 == 0 ) {
+            turn(square.target.id, huPlayer)
+            huPlayer_moves.push(square.target.id)
+            console.log(huPlayer_moves);
+        }else {
+            turn(square.target.id, aiPlayer)
+            aiPlayer_moves.push(square.target.id)
+        }
+        turn_number += 1;
+        check_winner(origBoard)
+        console.log(turn_number)
+        break;
+    }  
 }
+
+
 
 function turn(squareId, player) {
     origBoard[squareId] = player;
     document.getElementById(squareId).innerText = player;
+}
+
+function check_winner(board) {
+    if(board[0] === board[1] && board[1] === board[2]) { 
+        alert('we have a winner') 
+    } else if(board[0] === board[3] && board[3] === board[6]) {
+        alert('we have a winner') 
+    } else if(board[0] === board[4] && board[4] === board[8]) {
+        alert('we have a winner') 
+    } else if(board[3] === board[4] && board[4] === board[5]) {
+        alert('we have a winner') 
+    } else if(board[6] === board[7] && board[7] === board[8]) {
+        alert('we have a winner') 
+    } else if(board[2] === board[5] && board[5] === board[8]) {
+        alert('we have a winner') 
+    } else if(board[2] === board[4] && board[4] === board[6]) {
+        alert('we have a winner') 
+    }
 }
