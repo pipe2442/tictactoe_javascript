f0274128851362152f85e3fb761a629188a2847f
pipe2442 <felipeangulo2442@gmail.com>
2021-05-31 22:01:34 -0500

    Add replay option button

--- app.js
+++ app.js
@@ -6,44 +6,49 @@ function Player(name, marker) {
 }
 
 checkWinner = (board) => {
-  console.log('winner')
-}
+  console.log('winner');
+};
 
 validMove = () => {
   console.log('validMove');
-}
+};
 
+reload = () => {
+  name1.value = '';
+  name2.value = '';
+  location.reload();
+};
 
 startGame = () => {
-
+  document.getElementById('name1').style.visibility = 'hidden';
+  document.getElementById('name2').style.visibility = 'hidden';
   const player1 = new Player(document.getElementById('name1'), 'O');
   const player2 = new Player(document.getElementById('name2'), 'X');
   const cells = document.querySelectorAll('.cell');
   document.getElementById('game_table').style.visibility = 'visible';
+  document.getElementById('win').innerHTML = '';
 
   turn = (cell, player, next) => {
     document.getElementById('win').innerHTML = `<h1>It's ${next.name.value} turn</h1>`;
-    cell.innerText = player.marker
-  }
+    cell.innerText = player.marker;
+  };
 
   let turnNumber = 0;
   marker = (e) => {
     if (turnNumber % 2 === 0) {
-      turn(cells[e.target.id],player2, player1)
+      turn(cells[e.target.id], player2, player1);
     } else if (turnNumber % 2 !== 0) {
-      turn(cells[e.target.id],player1, player2)
+      turn(cells[e.target.id], player1, player2);
     }
-    turnNumber += 1
-    console.log(turnNumber) 
-    if(cells[1].innerText === cells[2].innerText){
-      alert('SE CUMPLIO!')
-      
+    turnNumber += 1;
+    console.log(turnNumber);
+    if (cells[1].innerText === cells[2].innerText) {
+      alert('SE CUMPLIO!');
     }
-  }
-  
+  };
+
   for (let i = 0; i < cells.length; i += 1) {
     cells[i].innerText = '';
     cells[i].addEventListener('click', marker);
   }
-  
-}
+};
