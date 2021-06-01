957e29891b4a2a9efe99f943e629d2e4b7d079e2
pipe2442 <felipeangulo2442@gmail.com>
2021-05-31 19:19:03 -0500

    Create startGame function - add turn with message function

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ validMove = () => {
 }
 
 
-
 startGame = () => {
 
   const player1 = new Player(document.getElementById('name1'), 'O');
@@ -22,20 +21,29 @@ startGame = () => {
   const cells = document.querySelectorAll('.cell');
   document.getElementById('game_table').style.visibility = 'visible';
 
-  turn = (cell, player) => {
-    board[cell] = player;
-    document.getElementById(cell).innerText = player;
+  turn = (cell, player, next) => {
+    document.getElementById('win').innerHTML = `<h1>It's ${next.name.value} turn</h1>`;
+    cell.innerText = player.marker
   }
 
+  let turnNumber = 0;
   marker = (e) => {
-    cells[e.target.id].innerText = 'X'
+    if (turnNumber % 2 === 0) {
+      turn(cells[e.target.id],player2, player1)
+    } else if (turnNumber % 2 !== 0) {
+      turn(cells[e.target.id],player1, player2)
+    }
+    turnNumber += 1
+    console.log(turnNumber) 
+    if(cells[1].innerText === cells[2].innerText){
+      alert('SE CUMPLIO!')
+      
+    }
   }
-
+  
   for (let i = 0; i < cells.length; i += 1) {
     cells[i].innerText = '';
     cells[i].addEventListener('click', marker);
   }
-
-
-
+  
 }
